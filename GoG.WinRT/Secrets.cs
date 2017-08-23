9f37b74239c68be3101948319ce5bfe6f11255cd
Chris Bordeman <cbordeman@outlook.com>
2017-08-23 09:04:27 -0400

    Changed infrastructure to a .Net Standard 1.3 library (anything higher is not supported by UWP apps) named GoG.Shared.  Changed models namespaces.  Updated Readme.md.

---
+++ GoG.WinRT/Secrets.cs
@@ -0,0 +1,11 @@
+﻿namespace GoG.WinRT
+{
+    public static class Secrets
+    {
+#if DEBUG
+        public const string GoHubUrl = "http://localhost:58388";
+#else
+        public const string GoHubUrl = "";
+#endif
+    }
+}
